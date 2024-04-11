74c34836bedcda779c1716b9c0c0b58d0267125a
Francesco_Fiorentino <francescofiorentino1996@gmail.com>
2024-04-11 18:44:43 +0200

    modifica

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navList = document.querySelector('.nav-list');
+
+  hamburgerMenu.addEventListener('click', function () {
+    navList.classList.toggle('show'); // Aggiunge o rimuove la classe 'show' quando viene cliccato l'hamburger menu
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
+    const bookListDiv = document.getElementById('bookList');
+    const searchButton = document.querySelector('button');
+  
+    searchButton.addEventListener('click', searchBooks);
+  
+    function searchBooks() {
+      const category = searchInput.value.toLowerCase();
+      const apiUrl = `https://openlibrary.org/subjects/${category}.json`;
+  
+      fetch(apiUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Errore nella richiesta');
+          }
+          return response.json();
+        })
+        .then(data => {
+          const books = data.works || [];
+          books.forEach(book => {
+            const title = book.title;
+            const authors = book.authors.map(author => author.name).join(', ');
+            const bookDiv = document.createElement('div');
+            bookDiv.classList.add('book-box');
+            bookDiv.innerHTML = `<p><strong>${title}</strong> - ${authors}</p>`;
+            bookDiv.addEventListener('mouseenter', () => bookDiv.classList.add('highlight'));
+            bookDiv.addEventListener('mouseleave', () => bookDiv.classList.remove('highlight'));
+            bookDiv.addEventListener('click', () => fetchBookDescription(book.key));
+            bookListDiv.classList.add('book-list-box');
+            bookListDiv.appendChild(bookDiv);
+          });
+        })
+        .catch(error => {
+          console.error('Si è verificato un errore:', error);
+        });
+    }
+  
+    function fetchBookDescription(key) {
+      const apiUrl = `https://openlibrary.org${key}.json`;
+  
+      fetch(apiUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Errore nella richiesta');
+          }
+          return response.json();
+        })
+        .then(data => {
+          const description = data.description || 'Descrizione non disponibile';
+          alert(description);
+        })
+        .catch(error => {
+          console.error('Si è verificato un errore:', error);
+        });
+    }
+  });
+  
+  
+  
+  
