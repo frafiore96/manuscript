522e5e2a8cc7e6d355fc8807fd74b546e41b1a0c
Francesco_Fiorentino <francescofiorentino1996@gmail.com>
2024-04-13 11:27:43 +0200

    Modifica 13-04-2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
+//Hamburger menu on mobile and tablet devices
 document.addEventListener('DOMContentLoaded', function () {
   const hamburgerMenu = document.querySelector('.hamburger-menu');
   const navList = document.querySelector('.nav-list');
 
   hamburgerMenu.addEventListener('click', function () {
-    navList.classList.toggle('show'); // Aggiunge o rimuove la classe 'show' quando viene cliccato l'hamburger menu
+    navList.classList.toggle('show'); // Add/remove'show' when hamburge menu is clicked
   });
 });
 
-
+//Search engine
 document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('searchInput');
     const bookListDiv = document.getElementById('bookList');
