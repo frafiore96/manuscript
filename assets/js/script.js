b80c790d970164e8a9971ec989ba21f7983041d3
Francesco_Fiorentino <francescofiorentino1996@gmail.com>
2024-04-21 12:58:23 +0200

    risoluzione bug, pulsante enter e loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-
-//Hamburger menu on mobile and tablet devices
-document.addEventListener('DOMContentLoaded', function () {
+// Hamburge menu creation
+  document.addEventListener('DOMContentLoaded', function () {
   const hamburgerMenu = document.querySelector('.hamburger-menu');
   const navList = document.querySelector('.nav-list');
 
@@ -9,37 +8,34 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-//Search engine
-  document.addEventListener('DOMContentLoaded', function () {
-  const searchInput = document.getElementById('searchInput');
+//Search functionality & input
+  document.addEventListener('DOMContentLoaded', function () { 
   const bookListDiv = document.getElementById('bookList');
   const searchButton = document.querySelector('button');
-  const loadingIndicator = document.getElementById('loading'); // Aggiungi l'elemento di caricamento
+  const loadingIndicator = document.getElementById('loading');
 
-  searchButton.addEventListener('click', searchBooks);
-
+  searchButton.addEventListener('click', searchBooks); 
   searchInput.addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
         searchBooks();
     }
   });
 
+//Search books function
   function searchBooks() {
-      const category = searchInput.value.toLowerCase();
+      const category = searchInput.value.toLowerCase(); 
       const apiUrl = `https://openlibrary.org/subjects/${category}.json`;
-
+      loadingIndicator.innerHTML = 'Caricamento...' //processing
+      loadingIndicator.style.display = 'block'; 
       bookListDiv.innerHTML = '';
-      loadingIndicator.innerHTML = 'Caricamento...'
-      loadingIndicator.style.display = 'block'; // Mostra l'indicatore di caricamento
-
-      fetch(apiUrl)
+      fetch(apiUrl) 
           .then(response => {
               if (!response.ok) {
                   throw new Error('Errore nella richiesta');
               }
               return response.json();
           })
-          .then(data => {
+          .then(data => { //output
               const books = data.works || [];
               books.forEach(book => {
                   const title = book.title;
@@ -53,37 +49,34 @@ document.addEventListener('DOMContentLoaded', function () {
                   bookListDiv.classList.add('book-list-box');
                   bookListDiv.appendChild(bookDiv);
               });
-              loadingIndicator.style.display = 'none'; // Nascondi l'indicatore di caricamento dopo il completamento
+              loadingIndicator.style.display = 'none'; 
           })
           .catch(error => {
               console.error('Si è verificato un errore:', error);
-              loadingIndicator.style.display = 'none'; // Assicurati che l'indicatore di caricamento venga nascosto anche in caso di errore
+              loadingIndicator.style.display = 'none'; 
           });
   }
-
+  
+//Book Desription function 
   function fetchBookDescription(key) {
       const apiUrl = `https://openlibrary.org${key}.json`;
 
-      fetch(apiUrl)
+      fetch(apiUrl) //processing
           .then(response => {
               if (!response.ok) {
                   throw new Error('Errore nella richiesta');
               }
               return response.json();
           })
-          .then(data => {
+          .then(data => { 
             let description = data.description;
-
-            // Verifica se la descrizione è un oggetto
             if (typeof description === 'object') {
-                // Se è un oggetto, converte la descrizione in una stringa
                 description = JSON.stringify(description);
             }
             else if (typeof description === 'string' && description.startsWith('http')) {
               description = 'Descrizione non disponibile';
             }
-            // Visualizza la descrizione
-            alert(description || 'Descrizione non disponibile');
+            alert(description || 'Descrizione non disponibile'); //output
         })
           .catch(error => {
               console.error('Si è verificato un errore:', error);
